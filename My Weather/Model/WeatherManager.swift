0d090ec1706d895ee04b6367632d9cf052d40a79
Setiawan Joddy <37212064+setiawanjoddy@users.noreply.github.com>
2021-04-01 17:54:26 +0700

    Adding some Networking Request

--- My Weather/Model/WeatherManager.swift
+++ My Weather/Model/WeatherManager.swift
@@ -7,3 +7,33 @@
 
 import Foundation
 
+struct WeatherManager {
+    static let apiToken = "ab0e24d79a4318f3fcfc141276255763"
+    static let measurementUnits = "metric"
+    let mainURL = "https://api.openweathermap.org/data/2.5/weather?&appid=\(apiToken)&units=\(measurementUnits)"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(mainURL)&q=\(cityName)"
+
+    }
+    
+    //Networking
+    func performRequest(with urlString: String) {
+        //Step 1. Create URL
+        if let url = URL(string: urlString) {
+            //Step 2. Create URL Session
+            let session = URLSession(configuration: .default)
+            //Step 3. Give the Session a "Task"
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    
+                }
+            }
+            //Step 4. Start the Task
+            task.resume()
+        }
+    }
+    
+    
+    
+}
